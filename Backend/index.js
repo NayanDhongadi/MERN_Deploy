d1b206727713edea0d029a474f77cf495a099f84
Nayan Jayantrao Dhongadi <99018458+NayanDhongadi@users.noreply.github.com>
2023-12-12 07:56:39 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,13 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 const mongoDB = require("./db")
 
+const corsOptions = {
+  origin: 'https://mern-deploy-frontend.vercel.app/',
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
+
 app.use(async(req,res,next)=>{
   await res.setHeader("Access-Control-Allow-Origin",process.env.REACT_APP_FRONTEND_URL);
   res.header(
