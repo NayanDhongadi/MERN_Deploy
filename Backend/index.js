7e4ea8b2749ff4ff3de48210147b8446e6379bae
Nayan Jayantrao Dhongadi <99018458+NayanDhongadi@users.noreply.github.com>
2023-12-12 08:00:55 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,17 +9,27 @@ const corsOptions = {
   origin: 'https://mern-deploy-frontend.vercel.app/',
   credentials: true,
 }
-
-app.use(cors(corsOptions))
-
-app.use(async(req,res,next)=>{
-  await res.setHeader("Access-Control-Allow-Origin",process.env.REACT_APP_FRONTEND_URL);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
-  next();
-});
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
 
 
 app.use(express.json());
